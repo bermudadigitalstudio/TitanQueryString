1c65e3c00f55bb14060df85140a612789657be8d
Swizzlr <git@swizzlr.co>
2017-01-27 16:08:50 -0500

    Break the API to make it more expressive.

--- Sources/TitanQueryString.swift
+++ Sources/TitanQueryString.swift
@@ -2,10 +2,17 @@ import TitanCore
 import Foundation
 
 public extension RequestType {
-  public var query: [(key: String, value: String)] {
+  public var queryPairs: [(key: String, value: String)] {
     guard let queryItems = URLComponents(string: self.path)?.queryItems else {
       return []
     }
     return queryItems.map { return (key: $0.name, value: $0.value ?? "") }
+  }
+  public var query: [String:String] {
+    var query: [String:String] = [:]
+    for (name, value) in self.queryPairs {
+      query[name] = value
+    }
+    return query
   }
 }
--- Tests/TitanQueryStringTests/TitanQueryStringTests.swift
+++ Tests/TitanQueryStringTests/TitanQueryStringTests.swift
@@ -2,26 +2,28 @@ import XCTest
 import TitanQueryString
 import TitanCore
 
-class TitanQueryStringTests: XCTestCase {
-    func testExample() {
-      let path = "/users?verified=true&q=thomas%20catterall"
-      let request: RequestType = Request(method: "GET", path: path, body: "", headers: [])
-      let parsedQuery = request.query
-      guard parsedQuery.count == 2 else {
-        XCTFail()
-        return
-      }
-      XCTAssertEqual(parsedQuery[0].key, "verified")
-      XCTAssertEqual(parsedQuery[0].value, "true")
-
-      XCTAssertEqual(parsedQuery[1].key, "q")
-      XCTAssertEqual(parsedQuery[1].value, "thomas catterall")
+final class TitanQueryStringTests: XCTestCase {
+  func testQueryPairs() {
+    let path = "/users?verified=true&q=thomas%20catterall"
+    let request: RequestType = Request(method: "GET", path: path, body: "", headers: [])
+    let parsedQuery = request.queryPairs
+    guard parsedQuery.count == 2 else {
+      XCTFail()
+      return
     }
+    XCTAssertEqual(parsedQuery[0].key, "verified")
+    XCTAssertEqual(parsedQuery[0].value, "true")
 
+    XCTAssertEqual(parsedQuery[1].key, "q")
+    XCTAssertEqual(parsedQuery[1].value, "thomas catterall")
+  }
+  func testQuery() {
+    let path = "/users?verified=true&q=thomas%20catterall&verified=false"
+    let request: RequestType = Request(method: "GET", path: path, body: "", headers: [])
+    let query = request.query
 
-    static var allTests : [(String, (TitanQueryStringTests) -> () throws -> Void)] {
-        return [
-            ("testExample", testExample),
-        ]
-    }
+    XCTAssertEqual(query["q"], "thomas catterall")
+    // Check last value wins
+    XCTAssertEqual(query["verified"], "false")
+  }
 }
