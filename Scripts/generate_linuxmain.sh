ebcb21af46b49d37feaf6c8de4b17ac20d6c3bca
Swizzlr <git@swizzlr.co>
2017-01-31 19:07:32 -0500

    Auto generate linux main.

---
+++ Scripts/generate_linuxmain.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SOURCERY_VERSION="$(sourcery --version)"
+if [ "$SOURCERY_VERSION" != "0.5.3" ]
+then
+  echo "You need sourcery 0.5.3 – please `brew install sourcery`"
+  exit 1
+fi
+
+SCRIPTS="`dirname \"$0\"`"
+TESTS="$SCRIPTS/../Tests"
+sourcery $TESTS $SCRIPTS/LinuxMain.stencil $TESTS
+mv $TESTS/LinuxMain.generated.swift $TESTS/LinuxMain.swift
--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,6 +1,18 @@
+// Generated using Sourcery 0.5.3 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
 import XCTest
-@testable import TitanQueryStringTests
+
+
+extension TitanQueryStringTests {
+  static var allTests = [
+    ("testQueryPairs", testQueryPairs),
+    ("testQuery", testQuery),
+  ]
+}
+
 
 XCTMain([
-     testCase(TitanQueryStringTests.allTests),
+  testCase(TitanQueryStringTests.allTests),
 ])
+
